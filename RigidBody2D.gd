f1ea554477c5cf30f232520ec37558630b5b0d45
GenericUserIsTaken <65835587+GenericUserIsTaken@users.noreply.github.com>
2023-12-03 12:56:02 -0800

    added draggable ball gaming

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -1,5 +1,8 @@
 extends RigidBody2D
 
+var mouseInside = false
+var mousePressed = false
+var shouldDrag = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +15,55 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	print(body)
+	#print(body)
+	#print(self.get_position())
+	#print(body.get_position())
+	pass
+	
+
+func _input(event):
+	if event.is_action_pressed("mouse"):
+		print("prssed")
+		mousePressed = true
+		if(mouseInside):
+			shouldDrag = true
+			self.set_gravity_scale(0)
+	elif event.is_action_released("mouse"):
+		print("released")
+		mousePressed = false
+		shouldDrag = false
+		self.set_gravity_scale(1)
+
+func _physics_process(delta):
+	var mouse_position = get_viewport().get_mouse_position()
+	var movementDir = mouse_position - self.position
+	if mousePressed and shouldDrag:
+		print("movementdir",movementDir)
+		#self.set_position(mouse_position)
+		self.apply_force(movementDir*(mouse_position.distance_to(self.position)))
+
+func _on_mouse_entered():
+	mouseInside = true
+	print("mouseinside")
+
+
+func _on_mouse_exited():
+	mouseInside = false
+	print("mouseinside")
+
+
+func _on_mouse_shape_entered(shape_idx):
+	mouseInside = true
+	print("mouseinside")
+
+
+func _on_mouse_shape_exited(shape_idx):
+	mouseInside = false
+	print("mouseinside")
+
+
+func _on_input_event(viewport, event, shape_idx):
+	print("Any input event")
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			print("Object clicked")
--- world_border.gd
+++ world_border.gd
@@ -6,6 +6,7 @@ extends StaticBody2D
 func _ready():
 	get_tree().get_root().size_changed.connect(_on_viewport_resize)
 	_on_viewport_resize()
+	self.owner.unfreezeBall()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,6 +17,6 @@ func _on_viewport_resize():
 	self.set_position(Vector2(newSize.x/2,newSize.y/2))
 	for child in self.get_children():
 		if child is CollisionShape2D and child.shape is WorldBoundaryShape2D:
-			child.shape.distance = -0.5 * (child.shape.normal.x*newSize.x+child.shape.normal.y*newSize.y)
+			child.shape.set_distance(0.5 * ((child.shape.normal.x*newSize.x)+(child.shape.normal.y*newSize.y)))
 			child.disabled = false
-			print(child.shape.distance)
+			#print(child.shape.normal,child.shape.distance)
