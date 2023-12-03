8588f6bee23ca8a7e188b50eb7ac95055ca690e3
GenericUserIsTaken <65835587+GenericUserIsTaken@users.noreply.github.com>
2023-12-02 17:34:21 -0800

    added gui and a cube demo game
    cube demo game goes hard fr fr

--- LoadingButton.gd
+++ LoadingButton.gd
@@ -3,13 +3,14 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var button = self
+	button.pressed.connect(self._button_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _button_pressed():
+	print("Loading:",sceneToLoad)
 	var next_scene = load(sceneToLoad)
 	get_tree().change_scene_to_packed(next_scene)
